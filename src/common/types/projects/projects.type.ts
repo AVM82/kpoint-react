fa0aa6da5ecf438805bac4395fb02e28a10a9863
steelbreath <zlatnikovyevgeniy@gmail.com>
2024-01-27 00:48:33 +0200

    Add back response mapping to ui components

---
+++ src/common/types/projects/projects.type.ts
@@ -0,0 +1,17 @@
+export type ProjectsType = {
+  projectId: string,
+  title: string,
+  summary: string,
+  owner: string
+  description: string,
+  logoImgUrl: string,
+  geoCoordinates: string,
+  createdAt: string,
+  state: string,
+  ownerSum: number,
+  collectedSum: number,
+  startSum: number,
+  collectDeadline: string,
+  goalSum: number,
+  goalDeadline: string,
+};
