3ead0c080344ee277e48f01795f6780c80ee5977
steelbreath <zlatnikovyevgeniy@gmail.com>
2024-01-29 18:42:34 +0200

    Edit project-card design

--- src/components/app/project-card.tsx
+++ src/components/app/project-card.tsx
@@ -1,8 +1,19 @@
+import ArrowForwardTwoToneIcon from '@mui/icons-material/ArrowForwardTwoTone';
+import BookmarkTwoToneIcon from '@mui/icons-material/BookmarkTwoTone';
+import ControlPointTwoToneIcon from '@mui/icons-material/ControlPointTwoTone';
+import MonetizationOnTwoToneIcon from '@mui/icons-material/MonetizationOnTwoTone';
+import PeopleAltTwoToneIcon from '@mui/icons-material/PeopleAltTwoTone';
+import ShareTwoToneIcon from '@mui/icons-material/ShareTwoTone';
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Chip from '@mui/material/Chip';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -19,12 +30,32 @@ const ProjectCard: FC<ProjectsProps> = ({ project_id, title, summary, logoImgUrl
   const { t } = useTranslation();
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 500 }}>
       <CardMedia
-        sx={{ height: 140 }}
+        sx={{ height: 200 }}
         image={ logoImgUrl }
-        title={ title }
-      />
+        title={ title }>
+        <Grid container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="flex-start">
+          <Grid item>
+            <Link  href="#" underline="none" color="black">
+              <BookmarkTwoToneIcon sx={{ margin: 1, color: 'blue' }}/></Link>
+            <Link  href="#" underline="none" color="black">
+              <ShareTwoToneIcon  sx={{ margin: 1, color: 'blue' }}/></Link>
+          </Grid>
+          <Grid item><ButtonGroup orientation="vertical" variant="text"
+            aria-label="outlined button group" sx={{ margin: 1 }}>
+            <Button size="small" startIcon={ <PeopleAltTwoToneIcon/> }
+              sx={{ justifyContent: 'right' }}>Допомогти</Button>
+            <Button size="small" startIcon={ <ControlPointTwoToneIcon/> }
+              sx={{ justifyContent: 'right' }}>Слідувати</Button>
+            <Button size="small" startIcon={ <MonetizationOnTwoToneIcon/> }
+              sx={{ justifyContent: 'right' }}>Донат</Button>
+          </ButtonGroup></Grid>
+        </Grid>
+      </CardMedia>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           { title }
@@ -34,8 +65,13 @@ const ProjectCard: FC<ProjectsProps> = ({ project_id, title, summary, logoImgUrl
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">{t('share_button')}</Button>
-        <Button size="small" href={'projects/'.concat(project_id)}>
+        <Stack direction="row" spacing={1}>
+          <Chip label="#java" />
+          <Chip label="#react" />
+          <Chip label="#postgres" />
+          <Chip label="#redux" />
+        </Stack>
+        <Button size="small" endIcon={ <ArrowForwardTwoToneIcon/> } href={'projects/'.concat(project_id)}>
           {t('learn_more_button')}
         </Button>
       </CardActions>
