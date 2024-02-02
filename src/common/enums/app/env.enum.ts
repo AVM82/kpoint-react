6a13005b5193368e8125286626771567502a91f9
Vitaliy Lipovetskiy <vitaliy1972li@gmail.com>
2024-02-02 18:35:10 +0200

    update form create project, add validate

--- src/common/enums/app/env.enum.ts
+++ src/common/enums/app/env.enum.ts
@@ -1,11 +1,9 @@
 const { REACT_APP_API_PATH } = process.env;
-const { OAUTH2_GOOGLE_CLIENT_ID } = process.env;
+const { REACT_APP_OAUTH2_GOOGLE_CLIENT_ID } = process.env;
 
 const ENV = {
   API_PATH: REACT_APP_API_PATH ?? '',
-  // OAUTH2_GOOGLE_CLIENT_ID: OAUTH2_GOOGLE_CLIENT_ID ?? '',
-  OAUTH2_GOOGLE_CLIENT_ID: '95773710437-per0n2kjs8v1438vahr6bp0htrka5bvm.apps.googleusercontent.com',
-
+  OAUTH2_GOOGLE_CLIENT_ID: REACT_APP_OAUTH2_GOOGLE_CLIENT_ID ?? '',
 } as const;
 
 export { ENV };
