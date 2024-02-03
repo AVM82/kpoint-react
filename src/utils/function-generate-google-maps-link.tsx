16f43291d6f2926c477e4fc682ddbee465907e5a
nikozka15 <vshyrshova@gmail.com>
2024-02-03 12:27:55 +0000

    CustomTabPanel, CircularProgress loader to substitute NoData, updated project page
    ci actual branch name

--- src/utils/function-generate-google-maps-link.tsx
+++ src/utils/function-generate-google-maps-link.tsx
@@ -1,8 +1,6 @@
 import { ProjectType } from '../common/types/projects/project.type';
 
 export function generateGoogleMapsLink(project: ProjectType): string {
-  // const { coordinates } = project;
-  // const { latitude, longitude } = coordinates;
 
   return `https://www.google.com/maps/place/${project.latitude},${project.longitude}`;
 }
