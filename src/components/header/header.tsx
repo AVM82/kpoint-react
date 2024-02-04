527bae6871781ed0f8bc98469d7fe78dd5a623b7
Max Skory <devisrush@gmail.com>
2024-02-04 18:10:48 +0200

    header buttons change was almost fixed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,25 +3,38 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
-import { FC } from 'react';
+import { FC , useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-interface LogInProps {
-  isLoggedIn: boolean;
-}
+import { StorageKey } from '../../common/enums/app/storage-key.enum';
+import { storage } from '../../services/services';
 
 const Header: FC = () => {
-
   const { t } = useTranslation();
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
-  function HeaderButtons(props: LogInProps): ReactJSXElement {
-    const isLoggedIn = props.isLoggedIn;
+  useEffect(() => {
+    const token = storage.getItem(StorageKey.TOKEN);
+    const user = storage.getItem(StorageKey.USER);
+    console.log(token);
+    console.log(user);
+    console.log(!!token);
+    console.log(!!user);
+    setIsLoggedIn(!!token && !!user);
+  }, []);
 
+  function handleLogout(): void {
+    storage.removeItem(StorageKey.TOKEN);
+    storage.removeItem(StorageKey.USER);
+    window.location.href = '/';
+  }
+
+  function HeaderButtons(): ReactJSXElement {
     if (isLoggedIn) {
       return (
         <>
-          <Button href="/sign-in" variant="outlined" sx={{ margin: 1 }}>{t('buttons.create_project')}</Button>
-          <Button href="/sign-up" variant="contained" sx={{ margin: 1 }}>{t('buttons.log_out')}</Button>
+          <Button href="/projects/new" variant="outlined" sx={{ margin: 1 }}>{t('buttons.create_project')}</Button>
+          <Button onClick={handleLogout} variant="contained" sx={{ margin: 1 }}>{t('buttons.log_out')}</Button>
         </>);
     }
 
@@ -40,15 +53,14 @@ const Header: FC = () => {
       alignItems="center"
       sx={{ background: 'lightgray' }}>
       <Grid item>
-        <Link  href="/" underline="none" color="black" sx={{ margin: 1 }}>{t('projects')}</Link>
-        <Link  href="#" underline="none" color="black" sx={{ margin: 1 }}>{t('about_us')}</Link>
-        <Link  href="/projects/new" underline="none" color="black" sx={{ margin: 1 }}>{t('new_project')}</Link>
+        <Link href="/" underline="none" color="black" sx={{ margin: 1 }}>{t('projects')}</Link>
+        <Link href="#" underline="none" color="black" sx={{ margin: 1 }}>{t('about_us')}</Link>
       </Grid>
       <Grid item>
         <Typography variant="h6" align="center">KEY POINTS</Typography>
       </Grid>
       <Grid item>
-        <HeaderButtons isLoggedIn={true}/>
+        <HeaderButtons/>
       </Grid>
     </Grid>
   );
